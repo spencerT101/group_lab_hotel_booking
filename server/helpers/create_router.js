e6626c28ee742e8a56ca3d4dc7e056c371302ed1
Spencer <Spencer.Taber@yahoo.com>
2021-05-19 12:08:07 +0100

    router connected to Mongodb -show all API working

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,9 +1,28 @@
+const express = require("express");
+const ObjectID = require("mongodb").ObjectID;
+
 const createRouter = function(collection){
 
+    const router = express.Router();
+
+
+    // SHOW ALL BOOKINGS
     router.get('/', (req, res)=>{
-        collection.find().toArray().then(data=> res.json(data)).then(result=> res.send(result))
+        collection.find().toArray()
+        .then(data=> res.json(data))
+        .then(result=> res.send(result))
         
     })
+
+
+    
+      // SHOW
+//   router.get('/:id', (req, res) => {
+//     const id = req.params.id;
+//     collection
+//       .findOne({_id: ObjectID(id)})
+//       .then(doc => res.json(doc))
+//   })
    
 
 
@@ -11,16 +30,23 @@ const createRouter = function(collection){
 
 
 // CREATE
-router.post('/', (req, res) =>)
+router.post('/', (req, res) => {})
 
 
 
 // DELETE
 
-    router.delete('/:id', (req, res) =>{
-        const deleteBooking = req.params.id
-        collection
-        .deleteOne({_id:ObjectID(deleteBooking)});
-        .then((docs) => res.json.(docs))
-    })
+    // router.delete('/:id', (req, res) =>{
+    //     const deleteBooking = req.params.id
+    //     collection
+    //     .deleteOne({_id:ObjectID(deleteBooking)});
+    //     // .then((docs) => res.json.(docs))
+    // })
+
+    //Update
+
+    
+    return router;
 }
+
+module.exports = createRouter;
--- server/server.js
+++ server/server.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const app = express();
 const MongoClient= require("mongodb").MongoClient;
-const createRouter = require("./helpers/create_router.js")
+const createRouter = require("./helpers/create_router")
 
 app.use(express.json())
 //connection established
-MongoClient.connect('mongodb://localhost:27017')
+MongoClient.connect("mongodb://localhost:27017")
 .then(client => {
     const db = client.db("guests");
     const bookingcollection = db.collection("bookings");
     const bookingRouter = createRouter(bookingcollection);
     app.use('/api/bookings', bookingRouter);
 })
-.catch(console.error);
+.catch(console.err);
 
 const cors = require('cors')
-app.use(corse());
+app.use(cors());
 
 //port
 app.listen(5000, function(){
-    this.address().port()  
+      
 })
